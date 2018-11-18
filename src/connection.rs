be0ad17f82f3bdfbc5b2b208771d3aeb780f241e
Ning Sun <sunng@protonmail.com>
2018-11-18 20:53:41 +0800

    (feat) better connection impl

--- src/connection.rs
+++ src/connection.rs
@@ -1,8 +1,8 @@
-use std::collections::VecDeque;
 use std::io;
 use std::net::SocketAddr;
-use std::sync::{Arc, RwLock};
 
+use futures::future;
+use futures::stream::SplitSink;
 use futures::sync::mpsc::{self, UnboundedSender};
 use futures::sync::oneshot::{self, Sender};
 use futures::{Future, Sink, Stream};
@@ -16,7 +16,8 @@ use protos::riemann::{Event, Msg};
 
 #[derive(Debug)]
 pub struct Connection {
-    sender_queue: UnboundedSender<(Msg, Sender<Msg>)>,
+    sender_queue: UnboundedSender<Sender<Msg>>,
+    socket_sender: SplitSink<Framed<TcpStream, MsgCodec>>,
     // callback_queue: VecDeque<Sender<Msg>>,
 }
 
@@ -24,39 +25,29 @@ impl Connection {
     pub fn connect(addr: &SocketAddr) -> impl Future<Item = Connection, Error = io::Error> {
         TcpStream::connect(addr).map(|socket| {
             let framed = Framed::new(socket, MsgCodec);
-            let (mut conn_sender, conn_receiver) = framed.split();
-            let callback_queue: Arc<RwLock<VecDeque<Sender<Msg>>>> =
-                Arc::new(RwLock::new(VecDeque::new()));
+            let (conn_sender, conn_receiver) = framed.split();
 
-            let cq1 = callback_queue.clone();
-            let f = conn_receiver
-                .for_each(move |frame| {
-                    if let Some(inner_tx) = cq1.write().unwrap().pop_front() {
-                        if let Err(e) = inner_tx.send(frame) {
-                            eprintln!("{:?}", e);
-                        };
-                    }
-                    Ok(())
+            let (cb_queue_tx, cb_queue_rx) = mpsc::unbounded::<Sender<Msg>>();
+
+            let receiver_loop = conn_receiver
+                .zip(
+                    cb_queue_rx
+                        .map_err(|_| io::Error::new(io::ErrorKind::Other, "callback queue error")),
+                )
+                .for_each(move |(frame, cb)| {
+                    cb.send(frame).map_err(|e| {
+                        io::Error::new(
+                            io::ErrorKind::Other,
+                            format!("failed to deliver msg to callback {:?}", e),
+                        )
+                    })
                 })
                 .map_err(|e| eprintln!("{:?}", e));
-            tokio::spawn(f);
+            tokio::spawn(receiver_loop);
 
-            let (sender_queue, receiver) = mpsc::unbounded();
-            let f2 = receiver.for_each(move |(msg, callback)| {
-                callback_queue.write().unwrap().push_back(callback);
-                if let Err(e) = conn_sender
-                    .start_send(msg)
-                    .and_then(|_| conn_sender.poll_complete())
-                {
-                    eprintln!("{:?}", e);
-                };
-                Ok(())
-            });
-            tokio::spawn(f2);
-
-            Client {
-                sender_queue,
-                // callback_queue,
+            Connection {
+                sender_queue: cb_queue_tx,
+                socket_sender: conn_sender,
             }
         })
     }
@@ -69,13 +60,18 @@ impl Connection {
         msg.set_events(RepeatedField::from_vec(events));
 
         let (tx, rx) = oneshot::channel::<Msg>();
-        if let Err(e) = self
+
+        let send_result = self
             .sender_queue
-            .start_send((msg, tx))
-            .and_then(|_| self.sender_queue.poll_complete())
-        {
-            eprintln!("{:?}", e);
-        };
-        rx.map_err(|e| io::Error::new(io::ErrorKind::UnexpectedEof, e))
+            .unbounded_send(tx)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+            .and_then(move |_| {
+                self.socket_sender
+                    .start_send(msg)
+                    .and_then(|_| self.socket_sender.poll_complete())
+            });
+
+        future::result(send_result)
+            .and_then(|_| rx.map_err(|e| io::Error::new(io::ErrorKind::UnexpectedEof, e)))
     }
 }
