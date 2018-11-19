3c4308af874a7444a5ecf485bf963b1e99391b5e
Ning Sun <sunng@protonmail.com>
2018-11-19 22:24:57 +0800

    (fix) no need for sub modules of tokio

--- examples/interactive.rs
+++ examples/interactive.rs
@@ -2,14 +2,12 @@ extern crate futures;
 extern crate protobuf;
 extern crate rustmann;
 extern crate tokio;
-extern crate tokio_codec;
-extern crate tokio_fs;
 
 use std::error::Error;
 
 use futures::{Future, Stream};
-use tokio_codec::{FramedRead, LinesCodec};
-use tokio_fs::stdin;
+use tokio::codec::{FramedRead, LinesCodec};
+use tokio::io::stdin;
 
 use protobuf::Chars;
 use rustmann::protos::riemann::Event;
