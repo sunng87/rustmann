e5c3613dde911c6738bd8ab6a9dd6dd8bba9411f
Ning Sun <sunng@protonmail.com>
2019-08-12 16:21:37 +0800

    (test) port example to tokio 0.2

--- examples/helloworld.rs
+++ examples/helloworld.rs
@@ -2,22 +2,18 @@
 
 use std::error::Error;
 
-use futures::future::{FutureExt, TryFutureExt};
 use protobuf::Chars;
 use rustmann::protos::riemann::Event;
 use rustmann::{Client, ClientOptions};
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let mut client = Client::new(&ClientOptions::default());
 
     let mut event = Event::new();
     event.set_service(Chars::from("test"));
 
-    let fut = async move {
-        let response = client.send_events(vec![event]).await;
-
-        println!("{:?}", response);
-    };
-    tokio::run(fut.unit_error().boxed().compat());
+    let response = client.send_events(vec![event]).await?;
+    println!("{:?}", response);
     Ok(())
 }
