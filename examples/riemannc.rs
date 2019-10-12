49b634a79e884b8a2c79ae3d19501ba77cc5e0fe
Ning Sun <sunng@protonmail.com>
2019-10-12 21:58:22 +0800

    (feat) add riemannc based on default option

--- examples/riemannc.rs
+++ examples/riemannc.rs
@@ -29,6 +29,42 @@ enum Opt {
 #[tokio::main]
 async fn main() -> Result<(), RiemannClientError> {
     let opt = Opt::from_args();
-    println!("{:?}", opt);
+
+    match opt {
+        Opt::Query{query} => {
+            let mut client = RiemannClient::new(&RiemannClientOptions::default());
+            let resp = client.send_query(query).await?;
+            println!("{:?}", resp);
+        },
+        Opt::Send{service, metric, host, time, state, ttl, tags} => {
+            let mut client = RiemannClient::new(&RiemannClientOptions::default());
+
+            let mut event_builder = EventBuilder::new();
+            event_builder = event_builder
+                .service(service)
+                .metric_d(metric);
+            if let Some(host) = host {
+                event_builder = event_builder.host(host);
+            }
+            if let Some(time) = time {
+                event_builder = event_builder.time(time);
+            }
+            if let Some(state) = state {
+                event_builder = event_builder.state(state);
+            }
+            if let Some(ttl) = ttl {
+                event_builder = event_builder.ttl(ttl);
+            }
+            if let Some(tags) = tags {
+                for t in tags.split(",") {
+                    event_builder = event_builder.add_tag(t);
+                }
+            }
+
+            let resp = client.send_events(vec![event_builder.build()]).await?;
+            println!("{:?}", resp);
+        }
+    }
+
     Ok(())
 }
