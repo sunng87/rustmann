b907e8f771fcfb41ac3e008d721140f8f622c735
Ning Sun <sunng@protonmail.com>
2019-10-18 00:11:00 +0800

    (doc) doc up

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,48 @@
+#![doc(html_root_url = "https://docs.rs/rustmann/")]
+//! # Rustmann
+//!
+//! Rustmann is the rust client for [riemann](https://riemann.io), a popular
+//! event aggregator and processor for distributed system.
+//!
+//! This implementation is based on tokio and using async-await style API.
+//!
+//! ## Features
+//!
+//! * Full async-await API
+//! * TCP/UDP/TLS transport support
+//! * Auto reconnect
+//! * Send and query API
+//! * EventBuilder
+//! * A usable Cli in example
+//!
+//! ## Quick Start
+//!
+//! ```
+//! use rustmann::{EventBuilder, RiemannClient, RiemannClientError, RiemannClientOptions};
+//!
+//! #[tokio::main]
+//! async fn main() -> Result<(), RiemannClientError> {
+//!     // create client with default configuration (to localhost:5555)
+//!     let mut client = RiemannClient::new(&RiemannClientOptions::default());
+//!
+//!     // create a riemann event using evnet builder API
+//!     let event = EventBuilder::new()
+//!         .service("riemann_test")
+//!         .state("ok")
+//!         .metric_f(123.4)
+//!         .build();
+//!
+//!     // send event to server
+//!     let response = client.send_events(vec![event]).await?;
+//!     println!("{:?}", response);
+//!
+//!     // query riemann
+//!     let query_response = client.send_query("service = \"riemann_test\"").await?;
+//!     println!("{:?}", query_response);
+//!     Ok(())
+//! }
+//! ```
+
 #[macro_use]
 extern crate failure;
 
