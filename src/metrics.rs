9d1646ab854d7def754aea2c6c7c68df471d0bb5
Ning Sun <sunng@protonmail.com>
2020-09-25 23:11:50 +0800

    (feat) wip riemann observer

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,12 +1,29 @@
 use metrics_core::{Builder, Drain, Observe, Observer};
 
-use crate::{RiemannClient, RiemannClientError};
+use crate::{Event, RiemannClient, RiemannClientError};
 
 pub struct RiemannExporter<B, C> {
     controller: C,
-    builder: B,
     client: RiemannClient,
 }
+
+pub struct RiemannObserver {
+    events: Vec<Event>,
+}
+
+pub struct RiemannObserverBuilder {}
+
+impl Builder for RiemannObserverBuilder {
+    type Output = RiemannObserver;
+
+    fn build(&self) -> Self::Output {
+        RiemannObserver
+    }
+}
+
+impl Observer for RiemannObserver {}
+
+impl Drain for RiemannObserver {}
 
 impl<B, C> RiemannExporter<B, C>
 where
@@ -14,31 +31,23 @@ where
     B: Builder + Send + Sync + 'static,
     B::Output: Drain<String> + Observer,
 {
-    pub fn new(client: RiemannClient) -> RiemannExporter {
-        RiemannExporter { client }
+    pub fn new(client: RiemannClient, builder: B, controller: C) -> RiemannExporter {
+        RiemannExporter {
+            client,
+            builder,
+            controller,
+        }
     }
 
     pub async fn start_async(self) -> Result<(), RiemannClientError> {
-        let builder = Arc::new(self.builder);
-        let controller = Arc::new(self.controller);
+        let observer = self.builder.build();
+        loop {
+            self.controller.observe(&mut observer);
+            let output = observer.drain();
 
-        let make_svc = make_service_fn(move |_| {
-            let builder = builder.clone();
-            let controller = controller.clone();
+            let event = into_event(output);
 
-            async move {
-                Ok::<_, Error>(service_fn(move |_| {
-                    let builder = builder.clone();
-                    let controller = controller.clone();
-
-                    async move {
-                        let mut observer = builder.build();
-                        controller.observe(&mut observer);
-                        let output = observer.drain();
-                        Ok::<_, Error>(Response::new(Body::from(output)))
-                    }
-                }))
-            }
-        });
+            self.client.send_events(vec![event]);
+        }
     }
 }
