d194f3cdd390e296c54498feb09608500f0757fc
Ning Sun <sunng@protonmail.com>
2020-12-07 23:06:50 +0800

    (feat) remove duplicate code and use waker api

--- src/state.rs
+++ src/state.rs
@@ -49,26 +49,10 @@ impl Future for Inner {
                 }
             },
             ClientState::Disconnected => {
-                let mut f = Transport::connect(self.options.clone()).boxed();
-
-                // FIXME: avoid dup code
-                match f.poll_unpin(cx) {
-                    Poll::Ready(Ok(conn)) => {
-                        // connected
-                        let conn = Arc::new(Mutex::new(conn));
-                        *inner_state = ClientState::Connected(conn.clone());
-                        Poll::Ready(Ok(conn))
-                    }
-                    Poll::Ready(Err(e)) => {
-                        // failed to connect, return error
-                        Poll::Ready(Err(RiemannClientError::from(e)))
-                    }
-                    Poll::Pending => {
-                        // still connecting
-                        *inner_state = ClientState::Connecting(f);
-                        Poll::Pending
-                    }
-                }
+                let f = Transport::connect(self.options.clone()).boxed();
+                *inner_state = ClientState::Connecting(f);
+                cx.waker().clone().wake();
+                Poll::Pending
             }
         }
     }
