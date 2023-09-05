10d329f21df1dc35fdf24b52ad68707ed29bb53b
Ning Sun <sunng@protonmail.com>
2023-09-05 15:29:16 +0800

    fix: resolve tls module compilation error

--- src/options.rs
+++ src/options.rs
@@ -27,7 +27,7 @@ pub struct RiemannClientOptions {
 #[cfg(feature = "tls")]
 fn default_tls_config() -> Arc<ClientConfig> {
     let mut root_cert_store = RootCertStore::empty();
-    root_cert_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
+    root_cert_store.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
         OwnedTrustAnchor::from_subject_spki_name_constraints(
             ta.subject,
             ta.spki,
